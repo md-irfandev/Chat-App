2481169a1aa270c3704ac7ad26ce5bb09b5699e5
Mohammad Irfan <143010252+md-irfandev@users.noreply.github.com>
2024-05-18 20:49:16 +0530

    Frontend signup & home pade updated

---
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -0,0 +1,13 @@
+
+const SideBar = () => {
+  return (
+    <div>
+      <SearchInput/>
+      <div className="divider px-3"></div>
+      {/* <Conversation/>
+      <LogoutButton/> */}
+    </div>
+  )
+}
+
+export default SideBar
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=' flex sm:h-[450px] md:h-[550px] rounded-lg bg-white-900  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 '>
+      <SideBar/>
+      
+    </div>
+  )
+}
+
+export default Home
